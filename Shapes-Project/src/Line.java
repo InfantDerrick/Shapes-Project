d9ac062000778779aaf84d988d6c2e0c64466b0c
Infant Derrick Gnanasusairaj <infant.derrick.gnanasusairaj@gmail.com>
2019-01-02 11:27:34 -0500

    jez

---
+++ Shapes-Project/src/Line.java
@@ -0,0 +1,16 @@
+
+public abstract class Line extends Shape {
+
+	@Override
+	public void paintOn(Screen screen, int xCoord, int yCoord) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void paintOn(Screen screen) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
