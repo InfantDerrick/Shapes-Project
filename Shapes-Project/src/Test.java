fa3f6b836c0caf7f8a829b0682dba8581c6ea813
Infant Derrick Gnanasusairaj <infant.derrick.gnanasusairaj@gmail.com>
2019-01-03 17:23:47 -0500

    cash money

--- Shapes-Project/src/Test.java
+++ Shapes-Project/src/Test.java
@@ -1,12 +1,20 @@
 public class Test {
-    public static void main(String args[]){
-        Screen s = new Screen(15,10,'.');
-        Shape x = new Frame('*', 6,6);
+    public static void main(String args[]) {
+        Screen s = new Screen(15, 10, 'A');
+        Shape x = new Frame('x', 6,6);
         Shape y = new VLine('j', 3);
         Shape oct = new Octagon('x', 3);
         oct.paintOn(s,3,0);
 //        x.paintOn(s);
 //        y.paintOn(s, 2,1);
+
+        x.paintOn(s);
+        Shape box = new Box('-', 4, 4);
+        Shape frame = new Frame('*', 4, 4);
+        box.paintOn(s, 0,0);
+        frame.paintOn(s, 5,5);
+        y.paintOn(s, 2,1);
+
         s.draw();
-    }
+}
 }
