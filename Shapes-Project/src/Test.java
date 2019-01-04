961591a6139a6a5f84fdd751d7263605966f4d1b
Infant Derrick Gnanasusairaj <infant.derrick.gnanasusairaj@gmail.com>
2019-01-04 00:34:26 -0500

    test files working properlly

--- Shapes-Project/src/Test.java
+++ Shapes-Project/src/Test.java
@@ -3,7 +3,7 @@ public class Test {
         Screen s = new Screen(40, 40, 'A');
         Shape x = new Frame('x', 6,6);
         Shape y = new VLine('j', 3);
-        Shape oct = new Octagon('x', 5);
+        Shape oct = new Octagon('g', 3);
         oct.paintOn(s,10,0);
 //        x.paintOn(s);
 //        y.paintOn(s, 2,1);
